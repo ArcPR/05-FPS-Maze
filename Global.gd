15d438f9e82f33f103024bb503640b3e52ba9a1e
ArcPR <arcprax@gmail.com>
2023-04-05 16:12:59 -0400

    commit

--- Global.gd
+++ Global.gd
@@ -6,7 +6,7 @@ func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
 func _unhandled_input(_event):
-	if Input.is_action_pressed("menu"):
+	if Input.is_action_just_pressed("menu"):
 		if menu == null:
 			menu = get_node_or_null("/root/Game/UI/Menu")
 		if menu != null:
